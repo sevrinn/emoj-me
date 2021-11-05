8a786f26ac901cfb03f8115712cedccffdcfa77d
Sevrinn <sevrinnsevrinn@gmail.com>
2021-11-05 16:07:08 -0700

    acreated add to front event on unshiftbtn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const emojiContainer = document.querySelector("#emoji-container")
 const pushBtn = document.querySelector("#push-btn")
+const unshiftBtn = document.querySelector("#unshift-btn")
+const shiftBtn = document.querySelector("#shift-btn")
+const popBtn = document.querySelector("#pop-btn")
 const emojiInput = document.querySelector("#emoji-input")
 let emojiArr = ["😜", "😏", "😈"]
 
@@ -17,6 +20,14 @@ renderEmojis()
 pushBtn.addEventListener("click", () => {
   if (emojiInput.value) {
     emojiArr.push(emojiInput.value)
+    emojiInput.value = ""
+    renderEmojis()
+  }
+})
+
+unshiftBtn.addEventListener("click", () => {
+  if (emojiInput.value) {
+    emojiArr.unshift(emojiInput.value)
     emojiInput.value = ""
     renderEmojis()
   }
